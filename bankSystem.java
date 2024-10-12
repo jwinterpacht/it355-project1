dfb1605f69338f118c116aa72298fa150a7189db
jwinterpacht <jadenwinterpacht@icloud.com>
2024-10-12 12:27:07 -0500

    Added comments demonstrating recommendation DCL50-J.

--- bankSystem.java
+++ bankSystem.java
@@ -40,6 +40,7 @@ import java.math.BigDecimal;
 
 
 class Customer implements Cloneable, Serializable {
+    // Recommendation DCL50-J: Use visually distinct identifiers
     private String name;
     private int[] accountBalances; // Stores multiple account balances
 
